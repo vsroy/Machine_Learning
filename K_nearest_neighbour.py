51fcd4fe2ec03fca31ece4e37ec6ee0b071b396b
vsroy <vishalshillong258@gmail.com>
2018-08-27 22:54:24 +0530

    Adding 2 files for K_nearest_neighbour and Logistic Regression

---
+++ K_nearest_neighbour.py
@@ -0,0 +1,13 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+sampleClassifier = KNeighborsClassifier(n_neighbors=3)
+
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+
+sampleClassifier.fit(X, Y)
+
+samplePrediction = sampleClassifier.predict([[190, 70, 43]])
+print(samplePrediction)
